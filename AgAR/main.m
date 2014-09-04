04f49429f5159f586461f037fcf4e31d6041e93c
mitrenegade <bobbyren@gmail.com>
2014-09-04 11:29:28 -0400

    Initial Commit

---
+++ AgAR/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AgAR
+//
+//  Created by Bobby Ren on 9/4/14.
+//  Copyright (c) 2014 BobbyRen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
