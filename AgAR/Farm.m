8493100a347ad22f3fac1e824fa5e4f3f2e19fe5
jren1963 <Jing@Jingqings-MacBook-Pro.local>
2014-09-06 16:45:38 -0400

    Added farms and fields
    - added core data model for farms and fields
    - added tableview to view fields
    - fields are sorted by farms

---
+++ AgAR/Farm.m
@@ -0,0 +1,18 @@
+//
+//  Farm.m
+//  AgAR
+//
+//  Created by Jingqing Ren on 9/6/14.
+//  Copyright (c) 2014 BobbyRen. All rights reserved.
+//
+
+#import "Farm.h"
+#import "Field.h"
+
+
+@implementation Farm
+
+@dynamic name;
+@dynamic fields;
+
+@end
---
+++ AgAR/FieldsTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  FieldsTableViewController.h
+//  AgAR
+//
+//  Created by Jingqing Ren on 9/6/14.
+//  Copyright (c) 2014 BobbyRen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FieldsTableViewController : UITableViewController
+{
+    NSFetchedResultsController *fieldFetcher;
+}
+@end
